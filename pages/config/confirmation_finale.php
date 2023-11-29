38de56a274aaae03b3fb319d7027cd2fd0a7cb2a
Saad2003HDE <144278701+Saad2003HDE@users.noreply.github.com>
2023-11-29 16:47:17 -0500

    Update confirmation_finale.php

--- pages/config/confirmation_finale.php
+++ pages/config/confirmation_finale.php
@@ -1,15 +1,27 @@
-
+<!-- confirmation.php -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- Définit l'encodage des caractères -->
     <meta charset="UTF-8">
+    
+    <!-- Permet une adaptation du contenu à la largeur de l'écran -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- Lien vers la feuille de style externe -->
     <link rel="stylesheet" href="style.css">
+    
+    <!-- Titre de la page affiché dans l'onglet du navigateur -->
     <title>Confirmation finale</title>
 </head>
 <body>
+    <!-- Conteneur principal de la page -->
     <div class="container">
+        
+        <!-- Titre principal de la page -->
         <h2>Confirmation finale</h2>
+        
+        <!-- Message de confirmation pour l'utilisateur -->
         <p>Merci, vos adresses ont été enregistrées avec succès !</p>
     </div>
 </body>
